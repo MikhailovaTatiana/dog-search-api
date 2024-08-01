480921063dc14cdfc4addf2d330bbe061b847839
Tatiana Mikhailova <imagetata@gmail.com>
2024-08-01 13:09:10 +0200

    populate all cards dynamically

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-// console.log("hello");
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    let dogs = data.message;
+    let breedNames = Object.keys(dogs);
+    console.log(typeof breedNames);
+    console.log(breedNames.length);
 
-// fetch('https://dog.ceo/api/breeds/list/all')
-//     .then((response) => response.json())
-//     .then((data) => console.log(data.message));
+    let breedCardRef = document.querySelector(".breed-name");
 
+    breedNames.forEach(breed => {
+        console.log("breeds ", breed);
+        let cardsContainer = document.querySelector('.download-cards');
 
-// fetch('https://dog.ceo/api/breeds/list/all')
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//         let dogs = data.message;
-//         console.log(dogs);
-//         Object.keys(dogs).forEach(dog => {
-//             document.body.innerHTML += `${dog}<br>`;
-//     })})
-//     .catch(e => console.log(e));
-
-fetch('https://dog.ceo/api/breeds/list/all')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        let dogs = data.message;
-        // console.log(Object.keys(dogs)[0]);
-        let breedCardRef = document.querySelector('.breed-name');
-        breedCardRef.innerHTML += `${Object.keys(dogs)[0]}`;
+        let card = document.createElement('div');
+        card.innerHTML = `
+            <section class="card-section">
+                <article class="breed-card">
+                    <figure class="breed-image-container">
+                        <img class="breed-image" src="./image.png" alt="breed's image">
+                    </figure>
+                    <h3 class="breed-name">Breed: ${breed}</h3>
+                </article>
+            </section>
+        `;
+        cardsContainer.appendChild(card);
     })
-    .catch(e => console.log(e));
-
-
-/* description.classList.add("description");
-description.textContent = `Breed: ${breed}`;
-card.appendChild(description);  */   
+  })
+  .catch((e) => console.log(e));
