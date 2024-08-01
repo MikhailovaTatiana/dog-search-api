2727c7c850741f51f70b7bcfe25d28d17779e632
Tatiana Mikhailova <imagetata@gmail.com>
2024-08-01 14:55:54 +0200

    fetch random images for each breed

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-fetch("https://dog.ceo/api/breeds/list/all")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    let dogs = data.message;
-    let breedNames = Object.keys(dogs);
-    console.log(typeof breedNames);
-    console.log(breedNames.length);
+setupPage();
 
-    let breedCardRef = document.querySelector(".breed-name");
-
-    breedNames.forEach(breed => {
-        console.log("breeds ", breed);
-        let cardsContainer = document.querySelector('.download-cards');
-
-        let card = document.createElement('div');
-        card.innerHTML = `
-            <section class="card-section">
-                <article class="breed-card">
-                    <figure class="breed-image-container">
-                        <img class="breed-image" src="./image.png" alt="breed's image">
-                    </figure>
-                    <h3 class="breed-name">Breed: ${breed}</h3>
-                </article>
-            </section>
-        `;
-        cardsContainer.appendChild(card);
-    })
-  })
-  .catch((e) => console.log(e));
+async function setupPage() {   
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then(data => {
+            let dogs = data.message;
+            let breedNames = Object.keys(dogs);
+            breedNames.forEach(breed => {
+                fetch(`https://dog.ceo/api/breed/${breed}/images`)
+                    .then(response => response.json())
+                    .then(data => {
+                        let cardsContainer = document.querySelector('.download-cards');
+                        let card = document.createElement('div');
+                        card.innerHTML = `
+                            <section class="card-section">
+                                <article class="breed-card">
+                                    <figure class="breed-image-container">
+                                        <img class="breed-image" src="${data.message[Math.floor(Math.random() * data.message.length)]}" alt="breed's image">
+                                    </figure>
+                                    <h3 class="breed-name">Breed: ${breed}</h3>
+                                </article>
+                            </section>
+                        `;
+                        cardsContainer.appendChild(card);
+                    })    
+            })
+        })
+        .catch((e) => console.log(e));
+}  
