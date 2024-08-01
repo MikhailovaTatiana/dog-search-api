6817c686332a975fe0e9641c2d56f8d1d7acd5f4
Tatiana Mikhailova <imagetata@gmail.com>
2024-08-01 11:05:34 +0200

    fetch of dog's breed on a card

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-console.log("hello");
+// console.log("hello");
 
 // fetch('https://dog.ceo/api/breeds/list/all')
 //     .then((response) => response.json())
 //     .then((data) => console.log(data.message));
+
+
+// fetch('https://dog.ceo/api/breeds/list/all')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         let dogs = data.message;
+//         console.log(dogs);
+//         Object.keys(dogs).forEach(dog => {
+//             document.body.innerHTML += `${dog}<br>`;
+//     })})
+//     .catch(e => console.log(e));
 
 fetch('https://dog.ceo/api/breeds/list/all')
     .then(response => response.json())
     .then(data => {
         console.log(data);
         let dogs = data.message;
-        console.log(dogs);
-        Object.keys(dogs).forEach(dog => {
-            document.body.innerHTML += `${dog}<br>`;
-    })})
+        // console.log(Object.keys(dogs)[0]);
+        let breedCardRef = document.querySelector('.breed-name');
+        breedCardRef.innerHTML += `${Object.keys(dogs)[0]}`;
+    })
     .catch(e => console.log(e));
+
+
+/* description.classList.add("description");
+description.textContent = `Breed: ${breed}`;
+card.appendChild(description);  */   
